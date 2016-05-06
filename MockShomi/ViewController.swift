f1ee2394ad416ac9b571163a710fc6c27be68b1d
Andrew <andrew@20xr.com>
2016-05-06 16:35:46 -0700

    first mock shame commit

---
+++ MockShomi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MockShomi
+//
+//  Created by aleker on 5/1/16.
+//  Copyright © 2016 aleker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
